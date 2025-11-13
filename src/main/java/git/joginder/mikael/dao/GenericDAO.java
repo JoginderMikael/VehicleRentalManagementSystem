7d8a4c8308ac5f1f3af7690f21f3ccb81402be98
Joginder Mikael <jogindermikael@gmail.com>
2025-11-13 17:36:26 +0300

    JPA CRUD LOGIC

--- src/main/java/git/joginder/mikael/dao/GenericDAO.java
+++ src/main/java/git/joginder/mikael/dao/GenericDAO.java
@@ -1,4 +1,41 @@
 package git.joginder.mikael.dao;
 
-public class GenericDAO {
+import git.joginder.mikael.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+//This is the reusable class with JPA CRUD LOGIC.
+public class GenericDAO<T> {
+    private final Class<T> type;
+    protected EntityManager em = JPAUtil.getEntityManager();
+    public GenericDAO(Class<T> type){
+        this.type = type;
+    }
+
+    public void save(T entity){
+        em.getTransaction().begin();
+        em.persist(entity);
+        em.getTransaction().commit();
+    }
+
+    public void update(T entity){
+        em.getTransaction().begin();
+        em.merge(entity);
+        em.getTransaction().commit();
+    }
+
+    public void delete(T entity){
+        em.getTransaction().begin();
+        em.remove(em.merge(entity));
+        em.getTransaction().commit();
+    }
+
+    public T findById(Long id){
+        return em.find(type, id);
+    }
+
+    public List<T> findAll(){
+        return em.createQuery("from " + type.getSimpleName(), type).getResultList();
+    }
 }
