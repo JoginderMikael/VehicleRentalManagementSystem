68972091d6de3a8e3e0d6360cefe67b04d7538f1
Joginder Mikael <jogindermikael@gmail.com>
2025-11-13 17:27:04 +0300

    utilities made

--- src/main/java/git/joginder/mikael/util/ConsoleUtil.java
+++ src/main/java/git/joginder/mikael/util/ConsoleUtil.java
@@ -1,4 +1,11 @@
 package git.joginder.mikael.util;
 
+import java.util.Scanner;
+
 public class ConsoleUtil {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static String read(String message){
+        System.out.println((message));
+        return scanner.nextLine();
+    }
 }
--- src/main/java/git/joginder/mikael/util/JPAUtil.java
+++ src/main/java/git/joginder/mikael/util/JPAUtil.java
@@ -1,4 +1,13 @@
 package git.joginder.mikael.util;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
 public class JPAUtil {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("vehicle_rental");
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
 }
